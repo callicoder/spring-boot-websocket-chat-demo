59f4e32b778b3c0720dc909768d690aa33db60e8
sanix <sanicksikani@gmail.com>
2020-12-20 21:41:46 +0300

    OK 2.0

--- src/main/java/com/example/websocketdemo/api/RestController.java
+++ src/main/java/com/example/websocketdemo/api/RestController.java
@@ -1,4 +1,34 @@
 package com.example.websocketdemo.api;
 
+import com.example.websocketdemo.models.Message;
+import com.example.websocketdemo.models.User;
+import com.example.websocketdemo.services.MessageService;
+import com.example.websocketdemo.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api")
 public class RestController {
+
+    private final UserService userService;
+    private final MessageService messageService;
+
+    public RestController(UserService userService, MessageService messageService) {
+        this.userService = userService;
+        this.messageService=messageService;
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userService.findAll();
+    }
+
+    @GetMapping("/messages")
+    List<Message> getMessages(){
+        return messageService.findAll();
+    }
+
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -39,7 +39,7 @@ function onConnected() {
     // Tell your username to the server
     stompClient.send("/app/chat.addUser",
         {},
-        JSON.stringify({sender: username, type: 'JOIN'})
+        JSON.stringify({username: username, type: 'JOIN'})
     )
 
     connectingElement.classList.add('hidden');
@@ -57,7 +57,7 @@ function sendMessage(event) {
 
     if(messageContent && stompClient) {
         var chatMessage = {
-            sender: username,
+            username: username,
             content: messageInput.value,
             type: 'CHAT'
         };
@@ -76,22 +76,22 @@ function onMessageReceived(payload) {
 
     if(message.type === 'JOIN') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' joined!';
+        message.content = message.username + ' joined!';
     } else if (message.type === 'LEAVE') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
+        message.content = message.username + ' left!';
     } else {
         messageElement.classList.add('chat-message');
 
         var avatarElement = document.createElement('i');
-        var avatarText = document.createTextNode(message.sender[0]);
+        var avatarText = document.createTextNode(message.author.username[0]);
         avatarElement.appendChild(avatarText);
-        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+        avatarElement.style['background-color'] = getAvatarColor(message.author.username);
 
         messageElement.appendChild(avatarElement);
 
         var usernameElement = document.createElement('span');
-        var usernameText = document.createTextNode(message.sender);
+        var usernameText = document.createTextNode(message.author.username);
         usernameElement.appendChild(usernameText);
         messageElement.appendChild(usernameElement);
     }
